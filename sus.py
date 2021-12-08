38027812bf379de6444fd54cb329adfe7275b81f
melegszendvics <94050521+melegszendvics@users.noreply.github.com>
2021-12-08 11:55:33 +0100

    sussy baka

---
+++ sus.py
@@ -0,0 +1,9 @@
+import os
+
+folder = input("Raw path: ")
+files = os.listdir(folder)
+count = 0
+for file in files:
+    if (file.startswith("sus")):
+        os.rename(str(folder+"\\"+file), str(folder+"\\sus "+str(count)+".png"))
+        count=count+1
